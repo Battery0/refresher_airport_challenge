4beb819ebafb217db020a211fcd093832ed2a0a4
ian <kickthechair@gmail.com>
2021-12-16 18:11:37 +0000

    second test passing - plane lands successfully into the airport hanger

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,8 +2,16 @@ require_relative '../lib/plane'
 
 class Airport
 
+  def initialize
+    @hanger = []
+  end
+
   def new_plane(plane = Plane.new)
-    @plane = plane
+    plane
+  end
+
+  def land_plane(plane)
+    @hanger << plane
   end
 
 end
--- spec/unit/airport_spec.rb
+++ spec/unit/airport_spec.rb
@@ -3,15 +3,21 @@ require 'airport'
 describe Airport do
 
   let(:airport) { described_class.new }
+  let(:plane_dbl) { double(:Plane) }
 
   describe '#new_plane' do
     it 'creates a new plane object' do
-      plane_dbl = double(:plane)
-      plane_class_dbl = class_double("Plane")
+      # plane_class_dbl = class_double("Plane")
       # allow(plane_class_dbl).to receive(:new).and_return(plane_dbl)
       # expect(airport.new_plane(plane_dbl)).to be_an_instance_of(plane_class_dbl)
       # need to check the above and see if its possible to mock/stub using "to be_an_instance_of" rspec syntax
       expect(airport.new_plane(plane_dbl)).to eq(plane_dbl)
+    end
+  end
+
+  describe '#land_plane' do
+    it 'tells a plane to land at the airport' do
+      expect(airport.land_plane(plane_dbl)).to eq([plane_dbl])
     end
   end
 
