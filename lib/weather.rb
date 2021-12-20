d66dc142e843164149334a70c849da91fe3f3cdf
ian <kickthechair@gmail.com>
2021-12-20 21:32:08 +0000

    first test passing for weather class - randomly returns sunny

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,3 +1,11 @@
 class Weather
 
+  def type
+    weather_generator = rand(0..15)
+
+    if weather_generator.between?(0, 3)
+      "sunny"
+    end
+  end
+
 end
--- spec/unit/weather_spec.rb
+++ spec/unit/weather_spec.rb
@@ -2,4 +2,13 @@ require 'weather'
 
 describe Weather do
 
+  let(:weather) { described_class.new }
+
+  describe '#type' do
+    it 'should randomly return sunny some of the time' do
+      allow(weather).to receive(:type) { "sunny" }
+      expect(weather.type).to eq("sunny")
+    end
+  end
+
 end
