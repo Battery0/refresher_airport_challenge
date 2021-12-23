fe1c10c64d0fdc02d73ff146549e05d5f54f25c7
ian <kickthechair@gmail.com>
2021-12-23 17:14:33 +0000

    refactored raising error message of the weather to private method

--- lib/airport.rb
+++ lib/airport.rb
@@ -18,7 +18,7 @@ class Airport
 
   def land_plane(plane)
     raise "This plane has already landed" unless plane.flying?
-    raise "The weather is currently stormy and planes can't take off" if @weather.type == "stormy"
+    weather_check
     airport_full_error
     plane.landed
     add_plane_to_hanger(plane)
@@ -26,13 +26,17 @@ class Airport
 
   def plane_take_off(plane)
     raise "That plane is not at the airport" unless plane_in_hanger?(plane)
-    raise "The weather is currently stormy and planes can't take off" if @weather.type == "stormy"
+    weather_check
     plane.taken_off
     puts "Plane #{plane} has taken off from the airport"
     @hanger.delete(plane)
   end
 
   private
+
+  def weather_check
+    raise "The weather is currently stormy and planes can't take off" if @weather.type == "stormy"
+  end
 
   def airport_full_error
     raise "The airport is currently full" if @hanger.length == @capacity
@@ -47,10 +51,3 @@ class Airport
   end
 
 end
-
-# ap = Airport.new
-# p1 = Plane.new
-# ap.add_new_plane(p1)
-# p ap.plane_take_off(p1)
-
-
